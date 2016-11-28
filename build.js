2f35e4e024df63c0e76f30fbbc4fbd596b5be8c1
Ricard Sole <ricardsolecasas@gmail.com>
2016-11-28 14:31:20 +0100

    First commit

---
+++ build.js
@@ -0,0 +1,25 @@
+const R = require('ramda')
+const nativeCSS = require('native-css')
+const cssRules = nativeCSS.convert('node_modules/basscss/css/basscss.css')
+
+const transformRuleName = (rule) => R.test(/\@media/, rule) ? rule : `.${R.replace(/_/g, '-', rule)}`
+
+const transformMediaQueries = (values) => {
+  if (R.has('__expression__', values)) {
+    return R.compose(
+      R.fromPairs,
+      R.map(([rule, vals]) => [transformRuleName(rule), vals]),
+      R.toPairs,
+      R.dissoc('__expression__')
+    )(values)
+  }
+
+  return values
+}
+const formatKeys = R.compose(
+  R.fromPairs,
+  R.map(([rule, values]) => [transformRuleName(rule), transformMediaQueries(values)]),
+  R.toPairs
+)
+
+console.log(JSON.stringify(formatKeys(cssRules), null, 2))
