2fc987ef8d4154c653a3e78cb655a6ec4bd0a8f6
Ricard Sole <ricardsolecasas@gmail.com>
2016-11-28 15:23:27 +0100

    provides build with and without dot

--- build.js
+++ build.js
@@ -3,14 +3,18 @@ const R = require('ramda')
 const nativeCSS = require('native-css')
 const cssRules = nativeCSS.convert('node_modules/basscss/css/basscss.css')
 
-const transformRuleName = (rule) =>
+const transformRuleDot = (rule) =>
   R.test(/\@media/, rule) ? rule : `.${R.replace(/_/g, '-', rule)}`
-
-const transformMediaQueries = (values) => {
+const transformRule = (rule) =>
+  R.test(/\@media/, rule) ? rule : R.replace(/_/g, '-', rule)
+const transformMediaQueries = (values, prependDot) => {
   if (R.has('__expression__', values)) {
     return R.compose(
       R.fromPairs,
-      R.map(([rule, vals]) => [transformRuleName(rule), vals]),
+      R.map(([rule, vals]) => [
+        prependDot ? transformRuleDot(rule) : transformRule(rule),
+        vals
+      ]),
       R.toPairs,
       R.dissoc('__expression__')
     )(values)
@@ -18,15 +22,22 @@ const transformMediaQueries = (values) => {
 
   return values
 }
-const formatKeys = R.compose(
+const dot = R.compose(
   R.fromPairs,
-  R.map(([rule, values]) => [transformRuleName(rule), transformMediaQueries(values)]),
+  R.map(([rule, values]) => [transformRuleDot(rule), transformMediaQueries(values, true)]),
   R.toPairs
-)
-
-const jsRules = formatKeys(cssRules)
+)(cssRules)
+const dotless = R.compose(
+  R.fromPairs,
+  R.map(([rule, values]) => [transformRule(rule), transformMediaQueries(values)]),
+  R.toPairs
+)(cssRules)
 
 fs.writeFileSync(
-  './index.js',
-  `module.exports = ${JSON.stringify(jsRules, null, 2)}\n`
+  './dot.js',
+  `module.exports = ${JSON.stringify(dot, null, 2)}\n`
+)
+fs.writeFileSync(
+  './dotless.js',
+  `module.exports = ${JSON.stringify(dotless, null, 2)}\n`
 )
